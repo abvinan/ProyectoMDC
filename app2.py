94c7f0d25813e68e10f9f6ba5add7b7bffc8a15e
abvinan <153153457+abvinan@users.noreply.github.com>
2024-11-03 17:08:45 -0500

    app2.py

--- app2.py
+++ app2.py
@@ -46,18 +46,15 @@ def obtener_top_200_productos(df_categoria):
         top_200_productos = df_categoria.groupby('COD_PRODUCTO')['CANTIDAD'].sum().nlargest(200).index
         return df_categoria[df_categoria['COD_PRODUCTO'].isin(top_200_productos)]
 
-# Preparar datos para entrenar el modelo ALS
 def preparar_datos_para_entrenar(df):
     if len(df) > 0:
         df_train, df_test = train_test_split(df, test_size=0.3, random_state=42)
         df_train_compras = df_train.groupby(['COD_FACTURA', 'COD_PRODUCTO'])['CANTIDAD'].sum().unstack().fillna(0)
-        df_test_compras = df_test.groupby(['COD_FACTURA', 'COD_PRODUCTO'])['CANTIDAD'].sum().unstack().fillna(0)
-        return df_train_compras, df_test_compras
+        return df_train_compras
     else:
         st.error("No hay suficientes datos para dividir en entrenamiento y prueba.")
-        return None, None
+        return None
 
-# Entrenar el modelo ALS
 def entrenar_modelo_als(df_train_compras):
     if df_train_compras is not None:
         df_train_sparse = csr_matrix(df_train_compras.values)
@@ -67,7 +64,6 @@ def entrenar_modelo_als(df_train_compras):
     else:
         return None, None
 
-# Generar recomendaciones para los productos seleccionados
 def generar_recomendaciones_seleccionados(df_train_compras, als_model, df_train_sparse, productos_seleccionados_ids):
     if df_train_compras is not None and als_model is not None:
         als_recommendations = {}
@@ -90,62 +86,35 @@ menu_seleccion = st.sidebar.radio("Seleccione una ventana:", ["Seleccionar Produ
 # Ventana 1: Selección de Productos
 if menu_seleccion == "Seleccionar Productos":
     st.header("Selecciona los Productos para Recomendación")
-
-    # Selección de categoría basada en el diccionario de secciones
     categoria_seleccionada = st.selectbox("Seleccione una Categoría", list(secciones.keys()))
-
-    # Filtrar el DataFrame para obtener solo los datos de la categoría seleccionada
     df_categoria = filtrar_por_categoria(df, categoria_seleccionada)
-
-    # Guardar df_categoria en el estado de la sesión
-    st.session_state['df_categoria'] = df_categoria
-
-    # Filtrar subcategorías según la categoría seleccionada
     subcategorias_disponibles = df_categoria['DESC_CLASE'].unique()
     subcategoria_seleccionada = st.selectbox("Seleccione una Subcategoría", subcategorias_disponibles)
-
-    # Filtrar productos según la subcategoría seleccionada
     productos_disponibles = df_categoria[df_categoria['DESC_CLASE'] == subcategoria_seleccionada]['DESC_PRODUCTO'].unique()
     productos_seleccionados = st.multiselect("Seleccione hasta 4 productos:", productos_disponibles, max_selections=4)
 
-    # Guardar la selección en el estado de sesión
+    if 'productos_seleccionados' not in st.session_state:
+        st.session_state.productos_seleccionados = []
     st.session_state.productos_seleccionados = productos_seleccionados
 
-    # Crear una tabla para mostrar los productos seleccionados sin el índice
     if st.session_state.productos_seleccionados:
         productos_seleccionados_df = pd.DataFrame({
             "Productos seleccionados": st.session_state.productos_seleccionados
         })
-        productos_seleccionados_df.index = [""] * len(productos_seleccionados_df)  # Eliminar el índice
+        productos_seleccionados_df.index = [""] * len(productos_seleccionados_df)
         st.table(productos_seleccionados_df)
-    else:
-        st.write("No se han seleccionado productos.")
 
 # Ventana 2: Mostrar Combos Recomendados
 elif menu_seleccion == "Recomendaciones":
     st.header("Combos Recomendados")
 
-    # Verificar que haya productos seleccionados y `df_categoria` esté definido en la sesión
-    if 'productos_seleccionados' in st.session_state and st.session_state.productos_seleccionados and 'df_categoria' in st.session_state:
-        df_categoria = st.session_state['df_categoria']
-
-        # Obtener los IDs de los productos seleccionados
-        productos_seleccionados_ids = [
-            df[df['DESC_PRODUCTO'] == nombre]['COD_PRODUCTO'].values[0]
-            for nombre in st.session_state.productos_seleccionados
-        ]
-
-        # Obtener el top 200 productos de la categoría seleccionada para entrenar el modelo
+    if 'productos_seleccionados' in st.session_state and st.session_state.productos_seleccionados:
+        productos_seleccionados_ids = [df[df['DESC_PRODUCTO'] == nombre]['COD_PRODUCTO'].values[0] for nombre in st.session_state.productos_seleccionados]
         df_top_200 = obtener_top_200_productos(df_categoria)
-
-        # Preparar los datos y entrenar el modelo ALS
-        df_train_compras, df_test_compras = preparar_datos_para_entrenar(df_top_200)
+        df_train_compras = preparar_datos_para_entrenar(df_top_200)
         modelo_als, df_train_sparse = entrenar_modelo_als(df_train_compras)
-
-        # Generar las recomendaciones para los productos seleccionados
         recomendaciones = generar_recomendaciones_seleccionados(df_train_compras, modelo_als, df_train_sparse, productos_seleccionados_ids)
 
-        # Mostrar las recomendaciones en una tabla
         combos = []
         for producto_id in productos_seleccionados_ids:
             descripcion_a = df[df['COD_PRODUCTO'] == producto_id]['DESC_PRODUCTO'].values[0]
@@ -155,19 +124,25 @@ elif menu_seleccion == "Recomendaciones":
                 precio_b = df[df['COD_PRODUCTO'] == recomendacion_id]['VALOR_PVSI'].values[0]
                 costo_a = df[df['COD_PRODUCTO'] == producto_id]['COSTO'].values[0]
                 costo_b = df[df['COD_PRODUCTO'] == recomendacion_id]['COSTO'].values[0]
-
                 precio_combo = precio_a + precio_b
                 margen_combo = round(((precio_combo - (costo_a + costo_b)) / precio_combo) * 100, 2)
-
                 combos.append({
                     'Producto A': descripcion_a,
                     'Producto B': descripcion_b,
                     'Precio Combo': f"${precio_combo:.2f}",
-                    'Margen Combo': f"{margen_combo}%"
+                    'Margen Combo': f"{margen_combo}%",
+                    'Seleccionado': st.checkbox(f"Seleccionar {descripcion_a} - {descripcion_b}", key=f"{producto_id}_{recomendacion_id}")
                 })
 
-        # Convertir la lista de combos en un DataFrame y mostrarla
         df_combos = pd.DataFrame(combos)
         st.table(df_combos)
+
+        st.session_state.combos_seleccionados = df_combos[df_combos['Seleccionado']]
+
+# Ventana 3: Resumen de Combos Seleccionados
+elif menu_seleccion == "Resumen de Combos Seleccionados":
+    st.header("Resumen de Combos Seleccionados")
+    if 'combos_seleccionados' in st.session_state and not st.session_state.combos_seleccionados.empty:
+        st.table(st.session_state.combos_seleccionados[['Producto A', 'Producto B', 'Precio Combo', 'Margen Combo']])
     else:
-        st.write("No se han seleccionado productos en la ventana anterior.")
+        st.write("No se han seleccionado combos.")
