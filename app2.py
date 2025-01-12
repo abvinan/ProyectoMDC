0c61cd89fde7c3efcd7cd80052deede49673806d
abvinan <153153457+abvinan@users.noreply.github.com>
2025-01-12 12:15:16 -0500

    app2.py

--- app2.py
+++ app2.py
@@ -26,7 +26,7 @@ st.markdown("""
     }
     .login-box {
         background: white;
-        padding: 10px;
+        padding: 20px;
         border-radius: 10px;
         box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
         width: 250px;
