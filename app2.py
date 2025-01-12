0ea43bb6af8746aa02dcc2c20d4564a68b1e43ce
abvinan <153153457+abvinan@users.noreply.github.com>
2025-01-12 13:57:37 -0500

    app2.py

--- app2.py
+++ app2.py
@@ -26,7 +26,6 @@ st.markdown("""
         background: white;
         padding: 20px;
         border-radius: 10px;
-        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
         width: 320px;
         text-align: left;
         margin-left: 180px;
