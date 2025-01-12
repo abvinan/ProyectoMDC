baff7a306ccc22e81267d75d2e211a818e49c3e2
abvinan <153153457+abvinan@users.noreply.github.com>
2025-01-12 14:35:59 -0500

    app2.py

--- app2.py
+++ app2.py
@@ -96,6 +96,20 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
+st.markdown("""
+    <style>
+    .test-label {
+        font-size: 24px !important;
+        font-weight: bold;
+        color: red;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+st.markdown('<label class="test-label">Usuario</label>', unsafe_allow_html=True)
+st.text_input("Usuario", placeholder="Ingrese su usuario")
+
+
 # Función para manejar la autenticación
 def autenticar_usuario():
     if "autenticado" not in st.session_state:
