4138c8b8a891667fae18d458db21dc2d9b93650f
abvinan <153153457+abvinan@users.noreply.github.com>
2025-01-12 12:15:49 -0500

    app2.py

--- app2.py
+++ app2.py
@@ -29,7 +29,7 @@ st.markdown("""
         padding: 20px;
         border-radius: 10px;
         box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-        width: 250px;
+        width: 500px;
         text-align: center;
         margin-left: auto;
     }
